e39d2284805b4ff12ce5ccc9fc808d0eeb761b4b
Junji <j7nj1.t41@gmail.com>
2025-05-24 10:57:30 -0300

    added fzf split bind, rg raw string mode

--- nvim/.config/nvim/lua/custom/fzf.lua
+++ nvim/.config/nvim/lua/custom/fzf.lua
@@ -41,7 +41,7 @@ local function live_grep()
     local fzf_extra = [[
     --no-hscroll \
     --ansi --phony --disabled --delimiter=':' \
-    --bind "change:reload:rg --color=always --hidden --line-number --smart-case -- {q} || true" \
+    --bind "change:reload:rg -F --color=always --hidden --line-number --smart-case -- {q} || true" \
     --preview 'bat \
       --paging=never --color=always --style=numbers,header,changes,grid \
       --highlight-line {2} --line-range {2}: {1}' \
@@ -208,7 +208,47 @@ local function find_files()
     vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
 end
 
+local function find_files_split()
+
+    -- Create scratch buf, open in floating win
+    local buf = vim.api.nvim_create_buf(false, true)
+    float_design.title = " Find Files "
+    float_design.title_pos = "center"
+    vim.api.nvim_open_win(buf, true, float_design)
+
+    -- Generate temp file for fzf selection
+    local temp_file = get_temp()
+
+    local cd_picker = "rg --files --hidden"
+
+    vim.fn.jobstart(cd_picker .. "|" .. fzf_default .. ">" .. temp_file, {
+            term = true,
+            on_exit = function(_, exit_code, _)
+                if exit_code == 0 then
+                    local fd = io.open(temp_file, "r")
+                    if fd then
+
+                        local selected = fd:read("*l")
+                        fd:close()
+                        vim.fn.delete(temp_file)
+
+                        vim.cmd("close")
+
+                        vim.cmd("vsp")
+                        vim.cmd("e " .. selected)
+                    end
+                else
+                    vim.cmd("close")
+                end
+            end
+        }
+    )
+    vim.api.nvim_feedkeys("i", "n", false)
+    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
+end
+
 vim.keymap.set("n", "<leader>fg", live_grep, {})
 vim.keymap.set("n", "<leader>fd", find_files, {})
+vim.keymap.set("n", "<leader>os", find_files_split, {})
 vim.keymap.set("n", "<leader>gs", git_status, {})
 vim.keymap.set("n", "<leader>en", edit_dotfiles, {})
