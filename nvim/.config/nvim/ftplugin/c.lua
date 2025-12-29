e4eb52e2d174c5bfed99c62e24727d1f643585e2
Junji <j7nj1.t41@gmail.com>
2025-12-29 15:07:14 -0500

    simplify fzf.lua

--- nvim/.config/nvim/ftplugin/c.lua
+++ nvim/.config/nvim/ftplugin/c.lua
@@ -1,7 +1,7 @@
 local set = require("custom.utils").set
 
+vim.opt.cindent = true
+
 -- TLPI macros
 set("<leader>er", "i<Tab><Tab>errExit(\"\");<Esc>hhi")
-
-vim.opt.cindent = true
 set("<leader>ii", "i#include \"tlpi_hdr.h\" // IWYU pragma: export<Esc>\"")
--- nvim/.config/nvim/lua/custom/fzf.lua
+++ nvim/.config/nvim/lua/custom/fzf.lua
@@ -1,5 +1,3 @@
-local utils = require("custom.utils")
-
 local fzf_default =
     "fzf --color=pointer:#E67E22,prompt:#E67E22 " ..
         "--prompt='> ' " ..
@@ -8,38 +6,75 @@ local fzf_default =
         "--preview-window=right:70%:wrap:noinfo " ..
         "--bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down "
 
-local float_width = vim.o.columns
-local float_height = vim.o.lines - 2 -- dont cover status bar
-
 local float_design = {
     style = "minimal",
     relative = "editor",
-    width = float_width,
-    height = float_height,
+    width = vim.o.columns,
+    height = vim.o.lines - 2, -- Don't cover status bar
     row = 0,
     col = 0,
     border = "none",
 }
 
-local function get_temp()
-    local temp_dir = vim.fn.stdpath("data") .. "/fzf_temp"
-    if vim.fn.isdirectory(temp_dir) == 0 then
-        vim.fn.mkdir(temp_dir, "p")
+local function start_fzf(picker, fzf_extra, edit_func)
+    fzf_extra = fzf_extra or ""
+    edit_func = edit_func or function(selection)
+        vim.cmd("e " .. selection)
     end
 
-    return temp_dir .. "/result-" .. vim.fn.getpid() .. ".txt"
+    -- Generate temp file for fzf selection
+    local temp_file = vim.fn.tempname()
+
+    vim.fn.jobstart(
+        picker .. "|" .. fzf_default .. fzf_extra .. ">" .. temp_file, {
+            term = true,
+            on_exit = function(_, exit_code, _)
+                if exit_code == 0 then
+                    local fd = io.open(temp_file, "r")
+                    if fd then
+                        local selection = fd:read("*l")
+                        fd:close()
+
+                        vim.cmd("close")
+
+                        edit_func(selection)
+                    end
+                else
+                    vim.cmd("close")
+                end
+            end
+        }
+    )
+    vim.api.nvim_feedkeys("i", "n", false)
+    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
+end
+
+local function open_term_win()
+    -- Create scratch buf, open in floating win
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    float_design.width = vim.o.columns
+    float_design.height = vim.o.lines - 2
+
+    vim.api.nvim_open_win(buf, true, float_design)
+end
+
+local function find_files()
+    open_term_win()
+    start_fzf("rg --files --hidden")
+end
+
+local function edit_nexus()
+    open_term_win()
+    start_fzf("rg --hidden --files ~/Nexus")
+end
+
+local function edit_dotfiles()
+    open_term_win()
+    start_fzf("rg --hidden --files ~/dotfiles")
 end
 
 local function live_grep()
-    --- Create scratch buf, open in floating win ---
-    local buf = vim.api.nvim_create_buf(false, true)
-    float_design.title = " Live Grep "
-    float_design.title_pos = "center"
-    vim.api.nvim_open_win(buf, true, float_design)
-
-    --- Generate temp file for fzf selection ---
-    local temp_file = get_temp()
-
     local fzf_extra = [[
     --no-hscroll \
     --ansi --phony --disabled --delimiter=':' \
@@ -49,120 +84,15 @@ local function live_grep()
       --highlight-line {2} --line-range {2}: {1}' \
     ]]
 
-    --- Execute fzf ---
-    vim.fn.jobstart(fzf_default .. fzf_extra .. ">" .. temp_file, {
-            term = true,
-            on_exit = function(_, exit_code, _)
-                if exit_code == 0 then
-                    local fd = io.open(temp_file, "r")
-                    if fd then
-
-                        local selected = fd:read("*l")
-                        fd:close()
-                        vim.fn.delete(temp_file)
-
-                        vim.cmd("close")
-
-                        vim.cmd("e " .. selected:match("[^:]+"))
-                        vim.cmd(selected:match(":(%d+)"))
-                        vim.cmd("normal! zz")
-                    end
-                else
-                    vim.cmd("close")
-                end
-            end
-        }
-    )
-    vim.api.nvim_feedkeys("i", "n", false)
-    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
-end
-
-local function edit_nexus()
-    --- Create scratch buf, open in floating win ---
-    local buf = vim.api.nvim_create_buf(false, true)
-    float_design.title = " Nexus "
-    float_design.title_pos = "center"
-    vim.api.nvim_open_win(buf, true, float_design)
-
-    --- Generate temp file for fzf selection ---
-    local temp_file = get_temp()
-
-    local dot_picker = "rg --hidden --files ~/Nexus"
-
-    --- Execute fzf ---
-    vim.fn.jobstart(dot_picker .. "|" .. fzf_default .. ">" .. temp_file, {
-            term = true,
-            on_exit = function(_, exit_code, _)
-                if exit_code == 0 then
-                    local fd = io.open(temp_file, "r")
-                    if fd then
-
-                        local selected = fd:read("*l")
-                        fd:close()
-                        vim.fn.delete(temp_file)
-
-                        vim.cmd("close")
-
-                        vim.cmd("e " .. selected)
-                    end
-                else
-                    vim.cmd("close")
-                end
-            end
-        }
-    )
-    vim.api.nvim_feedkeys("i", "n", false)
-    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
-end
-
-local function edit_dotfiles()
-    --- Create scratch buf, open in floating win ---
-    local buf = vim.api.nvim_create_buf(false, true)
-    float_design.title = " Dotfiles "
-    float_design.title_pos = "center"
-    vim.api.nvim_open_win(buf, true, float_design)
-
-    --- Generate temp file for fzf selection ---
-    local temp_file = get_temp()
-
-    local dot_picker = "rg --hidden --files ~/dotfiles"
-
-    --- Execute fzf ---
-    vim.fn.jobstart(dot_picker .. "|" .. fzf_default .. ">" .. temp_file, {
-            term = true,
-            on_exit = function(_, exit_code, _)
-                if exit_code == 0 then
-                    local fd = io.open(temp_file, "r")
-                    if fd then
-
-                        local selected = fd:read("*l")
-                        fd:close()
-                        vim.fn.delete(temp_file)
-
-                        vim.cmd("close")
-
-                        vim.cmd("e " .. selected)
-                    end
-                else
-                    vim.cmd("close")
-                end
-            end
-        }
-    )
-    vim.api.nvim_feedkeys("i", "n", false)
-    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
+    open_term_win()
+    start_fzf("ls", fzf_extra, function(selection)
+        vim.cmd("e " .. selection:match("[^:]+"))
+        vim.cmd(selection:match(":(%d+)"))
+        vim.cmd("normal! zz")
+    end)
 end
 
 local function git_status()
-    --- Create scratch buf, open in floating win ---
-    local buf = vim.api.nvim_create_buf(false, true)
-    float_design.title = " Git Status "
-    float_design.title_pos = "center"
-    vim.api.nvim_open_win(buf, true, float_design)
-
-    --- Generate temp file for fzf selection ---
-    local temp_file = get_temp()
-
     --- Get git root if exists ---
     local result = vim.system(
         { "git", "rev-parse", "--show-toplevel" },
@@ -183,163 +113,18 @@ local function git_status()
         "--preview='repo=" .. git_root ..
        [[; bat --style=changes --color=always "$repo"/{}' ]]
 
-    --- Execute fzf ---
-    vim.fn.jobstart(gs_picker .. "|" .. fzf_default .. fzf_extra .. ">" .. temp_file, {
-            term = true,
-            on_exit = function(_, exit_code, _)
-                if exit_code == 0 then
-                    local fd = io.open(temp_file, "r")
-                    if fd then
-
-                        local selected = fd:read("*l")
-                        fd:close()
-                        vim.fn.delete(temp_file)
-
-                        vim.cmd("close")
-
-                        vim.cmd("e " .. git_root .. "/" .. selected)
-                    end
-                else
-                    vim.cmd("close")
-                end
-            end
-        }
-    )
-    vim.api.nvim_feedkeys("i", "n", false)
-    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
+    open_term_win()
+    start_fzf(gs_picker, fzf_extra, function(selected)
+        vim.cmd("e " .. git_root .. "/" .. selected)
+    end)
 end
 
-local function find_files()
+-- TO ADD
 
-    -- Create scratch buf, open in floating win
-    local buf = vim.api.nvim_create_buf(false, true)
-    float_design.title = " Find Files "
-    float_design.title_pos = "center"
-    vim.api.nvim_open_win(buf, true, float_design)
+-- Command for buffers
 
-    -- Generate temp file for fzf selection
-    local temp_file = get_temp()
-
-    local cd_picker = "rg --files --hidden"
-
-    vim.fn.jobstart(cd_picker .. "|" .. fzf_default .. ">" .. temp_file, {
-            term = true,
-            on_exit = function(_, exit_code, _)
-                if exit_code == 0 then
-                    local fd = io.open(temp_file, "r")
-                    if fd then
-
-                        local selected = fd:read("*l")
-                        fd:close()
-                        vim.fn.delete(temp_file)
-
-                        vim.cmd("close")
-
-                        vim.cmd("e " .. selected)
-                    end
-                else
-                    vim.cmd("close")
-                end
-            end
-        }
-    )
-    vim.api.nvim_feedkeys("i", "n", false)
-    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
-end
-
-local function find_files_split()
-
-    -- Create scratch buf, open in floating win
-    local buf = vim.api.nvim_create_buf(false, true)
-    float_design.title = " Find Files "
-    float_design.title_pos = "center"
-    vim.api.nvim_open_win(buf, true, float_design)
-
-    -- Generate temp file for fzf selection
-    local temp_file = get_temp()
-
-    local cd_picker = "rg --files --hidden"
-
-    vim.fn.jobstart(cd_picker .. "|" .. fzf_default .. ">" .. temp_file, {
-            term = true,
-            on_exit = function(_, exit_code, _)
-                if exit_code == 0 then
-                    local fd = io.open(temp_file, "r")
-                    if fd then
-
-                        local selected = fd:read("*l")
-                        fd:close()
-                        vim.fn.delete(temp_file)
-
-                        vim.cmd("close")
-
-                        vim.cmd("vsp")
-                        utils.c_cmd("<C-w>L")
-
-                        vim.cmd("e " .. selected)
-                    end
-                else
-                    vim.cmd("close")
-                end
-            end
-        }
-    )
-    vim.api.nvim_feedkeys("i", "n", false)
-    vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
-end
-
--- local buffers = vim.api.nvim_list_bufs()
--- for _, buf in ipairs(buffers) do
---     if vim.api.nvim_buf_is_loaded(buf) then
---         print(vim.api.nvim_buf_get_name(buf))
---     end
--- end
---
--- local function find_files_buf()
---
---     -- Create scratch buf, open in floating win
---     local buf = vim.api.nvim_create_buf(false, true)
---     float_design.title = " Find Files "
---     float_design.title_pos = "center"
---     vim.api.nvim_open_win(buf, true, float_design)
---
---     -- Generate temp file for fzf selection
---     local temp_file = get_temp()
---
---     local cd_picker = "rg --files --hidden"
---
---     vim.fn.jobstart(cd_picker .. "|" .. fzf_default .. ">" .. temp_file, {
---             term = true,
---             on_exit = function(_, exit_code, _)
---                 if exit_code == 0 then
---                     local fd = io.open(temp_file, "r")
---                     if fd then
---
---                         local selected = fd:read("*l")
---                         fd:close()
---                         vim.fn.delete(temp_file)
---
---                         vim.cmd("close")
---
---                         vim.cmd("vsp")
---                         utils.c_cmd("<C-w>L")
---
---                         vim.cmd("e " .. selected)
---                     end
---                 else
---                     vim.cmd("close")
---                 end
---             end
---         }
---     )
---     vim.api.nvim_feedkeys("i", "n", false)
---     vim.keymap.set("t", "<ESC>", function() vim.cmd("close") end, { buffer = 0 })
--- end
-
-vim.keymap.set("n", "<leader>fg", live_grep, {})
 vim.keymap.set("n", "<leader>fd", find_files, {})
-vim.keymap.set("n", "<leader>os", find_files_split, {})
--- vim.keymap.set("n", "<leader>os", find_files_buf, {})
-vim.keymap.set("n", "<leader>gs", git_status, {})
 vim.keymap.set("n", "<leader>en", edit_dotfiles, {})
 vim.keymap.set("n", "<leader>eo", edit_nexus, {})
+vim.keymap.set("n", "<leader>fg", live_grep, {})
+vim.keymap.set("n", "<leader>gs", git_status, {})
